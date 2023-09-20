edee445d7867d4e2c8b46955be88870fd5b19136
WillianDomingues79 <willian.domingues3@gmail.com>
2023-09-20 20:19:23 -0300

    feat:Inclusão do calculo de autonomia

--- app/src/main/java/com/example/eletriccarapp/MainActivity.kt
+++ app/src/main/java/com/example/eletriccarapp/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.eletriccarapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.eletriccarapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener{
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnCalc.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.btnCalc){
+            calcularAutonomia()
+        }
+    }
+
+    fun calcularAutonomia() {
+        var preco = binding.pricekwh.text.toString().toFloat()
+        var kmPercorrido = binding.totalKM.text.toString().toFloat()
+        var resultado = preco / kmPercorrido
+
+        binding.result.text = resultado.toString()
+        Toast.makeText(this,"Calculado", Toast.LENGTH_LONG).show()
     }
 }
